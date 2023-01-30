7e7154ec71bf38b92d3ebd8bafa45fd75cd4aec0
twstagg <tristinstagg@protonmail.com>
2023-01-30 17:07:15 -0600

    Add Harmony Patch that sets ExitMapGrid.Color.a = 0 on vacuum maps, allowing full transparency for preset vanilla green overlay present on perimeter of vacuum map

---
+++ Source/Universum/Universum/Utilities/Vacuum.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Universum.Utilities {
+    /**
+     * Sets ExitMapGrid.Color.a = 0 on vacuum maps, allowing full transparency for preset vanilla green overlay present on perimeter of vacuum map
+     */
+    [HarmonyLib.HarmonyPatch(typeof(Verse.ExitMapGrid), "Color", HarmonyLib.MethodType.Getter)]
+    public static class ExitMapGrid_Color {
+        public static void Postfix(ref Color __result) {
+            if (!Cache.allowed_utility(Verse.Find.CurrentMap, "Universum.vacuum")) return;
+            __result.a = 0;
+        }
+    }
+}
