767cc4a50339b5ebd3628fc022c9866259fbb45f
Sindre Eiklid <sindre0830@gmail.com>
2023-08-03 11:47:32 +0200

    Add support for different colors of vacuum overlay

--- Source/Universum/Universum/Globals.cs
+++ Source/Universum/Universum/Globals.cs
@@ -7,7 +7,12 @@ namespace Universum {
         public static Texture2D planet_screenshot = new Texture2D(2048, 2048, TextureFormat.RGB24, false);
         public static RenderTexture render = new RenderTexture(2048, 2048, 16);
         public static Material planet_mat = MaterialPool.MatFrom(planet_screenshot);
+        public static Material planet_mat_glass = new Material(planet_mat);
         public static bool rendered = false;
         public static float planet_render_altitude = 1100f;
+
+        static Globals() {
+            planet_mat_glass.color = Color.gray;
+        }
     }
 }
