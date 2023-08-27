57e8b433ac63ced8962eed4f606b0d8f587b732c
Sindre Eiklid <sindre0830@gmail.com>
2023-08-27 23:03:17 +0200

    Add Material def

---
+++ Source/Universum/Universum/Defs/Material.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace Universum.Defs {
+    public class Material : Def {
+        public string shaderName;
+        public string texturePath;
+        public Color color = Color.white;
+        public int renderQueue = RimWorld.Planet.WorldMaterials.WorldObjectRenderQueue;
+    }
+}
