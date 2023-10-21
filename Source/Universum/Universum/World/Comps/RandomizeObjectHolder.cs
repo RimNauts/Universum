6171ce601bf1be4a465d782a8a5e5824a48fef18
Sindre Eiklid <sindre0830@gmail.com>
2023-10-21 23:22:16 +0200

    Add `RandomizeObjectHolder` world object comp

---
+++ Source/Universum/Universum/World/Comps/RandomizeObjectHolder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace Universum.World.Comps {
+    public class RandomizeObjectHolder_Properties : RimWorld.WorldObjectCompProperties {
+        public string label;
+        public string desc;
+
+        public RandomizeObjectHolder_Properties() => compClass = typeof(RandomizeObjectHolder);
+    }
+
+    public class RandomizeObjectHolder : RimWorld.Planet.WorldObjectComp {
+        public RandomizeObjectHolder_Properties Props => (RandomizeObjectHolder_Properties) props;
+
+        public override IEnumerable<Gizmo> GetGizmos() {
+            if (DebugSettings.godMode) {
+                yield return new Command_Action {
+                    defaultLabel = Props.label,
+                    defaultDesc = Props.desc,
+                    action = RandomizeObject,
+                };
+            }
+        }
+
+        public void RandomizeObject() {
+            ObjectHolder objectHolder = this.parent as ObjectHolder;
+            objectHolder.Randomize();
+        }
+    }
+}
