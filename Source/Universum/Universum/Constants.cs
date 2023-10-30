abab41a8e48fcbcdfc42b137497f7ec7590833cf
Sindre Eiklid <sindre0830@gmail.com>
2023-10-30 10:44:05 +0100

    Bump version

--- Source/Universum/Universum/Constants.cs
+++ Source/Universum/Universum/Constants.cs
@@ -11,7 +11,7 @@
 
     public static class Info {
         public static readonly string name = "Universum";
-        public static readonly string version = "1.10.1";
+        public static readonly string version = "2.0.0";
     }
 
     public static class Style {
