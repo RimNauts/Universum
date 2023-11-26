ec66a9ce9852080bd3516c4494a0ea3a9d46934e
Sindre Eiklid <sindre0830@gmail.com>
2023-11-26 17:57:08 +0100

    Fix syntax error

--- Source/Universum/Universum/Utils.cs
+++ Source/Universum/Universum/Utils.cs
@@ -3,7 +3,7 @@
 namespace Universum {
     public static class Utils {
         public static Quaternion billboardRotation() {
-            return Quaternion.LookRotation(Game.MainLoop.instance.cameraForward, Vector3.up); ;
+            return Quaternion.LookRotation(Game.MainLoop.instance.cameraForward, Vector3.up);
         }
     }
 }
